ee29c323fe6cc3682ae9e926953542ec051f8d31
Qiu Xiang <i@7c00.cc>
2017-02-05 17:36:31 +0800

    Add Navbar

--- ebank/home/__init__.py
+++ ebank/home/__init__.py
@@ -1,5 +1,29 @@
-from flask import render_template
+# coding=utf-8
+
+from flask import request, render_template
 from .app import app
+
+@app.context_processor
+def context_processor():
+    return {
+        'navigation': [
+            {
+                'title': '首页',
+                'url': '/',
+                'active': request.path == '/',
+            },
+            {
+                'title': '现金服务',
+                'url': '/a',
+                'active': request.path == 'a',
+            },
+            {
+                'title': '预算管理',
+                'url': '/b',
+                'active': request.path == 'b',
+            },
+        ]
+    }
 
 @app.route('/')
 def index():
