32912b004fabceafb748f2499e7454937acc313d
7c00 <i@7c00.cc>
2017-02-10 01:34:54 +0800

    Add sms module

--- ebank/config.py
+++ ebank/config.py
@@ -1,5 +1,10 @@
+# coding=utf-8
 import os
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'SECRET_KEY')
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///ebank.sqlite')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+SMS_ACCESS_KEY = 'LTAI7M50j5C6POdg'
+SMS_SECRET_KEY = 'GPd3SMAIX6jRnJRTZW3M15byz3u4Vp'
+SMS_SIGN_NAME = '易伴金服'
---
+++ ebank/sms.py
@@ -0,0 +1,46 @@
+# coding=utf-8
+import base64
+import hashlib
+import hmac
+import urllib
+import uuid
+from collections import OrderedDict
+from datetime import datetime
+
+import requests
+
+
+class Sms:
+    def __init__(self, sign_name='', access_key='', secret_key=''):
+        self.sign_name = sign_name
+        self.access_key = access_key
+        self.secret_key = secret_key
+
+    def init_app(self, app):
+        self.__init__(
+            app.config['SMS_SIGN_NAME'],
+            app.config['SMS_ACCESS_KEY'],
+            app.config['SMS_SECRET_KEY'])
+
+    def sign(self, params):
+        string = 'POST&%2F&' + urllib.quote_plus(urllib.urlencode(params))
+        params['Signature'] = base64.encodestring(hmac.new(
+            self.secret_key + '&', string, hashlib.sha1).digest()).strip()
+        return params
+
+    def send(self, mobile, template_code, param):
+        params = self.sign(OrderedDict([
+            ('AccessKeyId', self.access_key),
+            ('Action', 'SingleSendSms'),
+            ('Format', 'JSON'),
+            ('ParamString', param),
+            ('RecNum', mobile),
+            ('SignName', self.sign_name),
+            ('SignatureMethod', 'HMAC-SHA1'),
+            ('SignatureNonce', uuid.uuid4()),
+            ('SignatureVersion', '1.0'),
+            ('TemplateCode', template_code),
+            ('Timestamp', datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')),
+            ('Version', '2016-09-27'),
+        ]))
+        return requests.post('https://sms.aliyuncs.com', data=params).json()
