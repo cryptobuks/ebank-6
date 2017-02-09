51670dd6839dc21186e8d30ec73971357fadbc62
7c00 <i@7c00.cc>
2017-02-09 21:59:59 +0800

    Add login layout

--- ebank/enterprise/assets/components/login.jsx
+++ ebank/enterprise/assets/components/login.jsx
@@ -2,6 +2,33 @@ import React from 'react'
 
 export default class Login extends React.Component {
   render() {
-    return <h1>login</h1>
+    return <form className="col-lg-4 col-lg-offset-4">
+      <div className="page-header">
+        <h1>eBank</h1>
+      </div>
+      <div className="form-group">
+        <div className="input-group">
+          <label className="input-group-addon" htmlFor="user">
+            <span className="glyphicon glyphicon-user"/>
+          </label>
+          <input type="text" className="form-control" id="user" placeholder="用户名"/>
+        </div>
+      </div>
+      <div className="form-group">
+        <div className="input-group">
+          <label className="input-group-addon" htmlFor="password">
+            <span className="glyphicon glyphicon-lock"/>
+          </label>
+          <input type="text" className="form-control" id="password" placeholder="密码"/>
+        </div>
+      </div>
+      <div className="form-group">
+        <button className="btn btn-block btn-primary">登录</button>
+      </div>
+      <div className="form-group text-center">
+        <a href="#">忘记密码？</a>&nbsp;&nbsp;
+        <a href="#"> 注册新账号</a>
+      </div>
+    </form>
   }
 }
--- ebank/enterprise/assets/main.jsx
+++ ebank/enterprise/assets/main.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import Main from './components/main'
 import 'bootstrap/dist/css/bootstrap'
-import './style'
+import './styles/login'
 
 ReactDom.render(<Main/> , document.getElementById('main'))
