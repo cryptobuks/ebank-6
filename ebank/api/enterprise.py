d44b4d92a5a011bf98c3c68201db4ba84394c641
7c00 <i@7c00.cc>
2017-02-10 23:59:08 +0800

    简单实现注册接口

--- ebank/api/enterprise.py
+++ ebank/api/enterprise.py
@@ -1,9 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from ..model import sqlalchemy
+from ..model.enterprise import Enterprise
 from .utils import response_success
 
 blueprint = Blueprint('client', __name__, url_prefix='/enterprise')
 
 
-@blueprint.route('/')
+@blueprint.route('/register', methods=['POST'])
 def index():
-    return response_success('hello')
+    enterprise = Enterprise()
+    enterprise.business_id = request.form['business-id']
+    enterprise.business_name = request.form['business-name']
+    enterprise.contacts_name = request.form['contacts-name']
+    enterprise.contacts_mobile = request.form['contacts-mobile']
+    enterprise.account = request.form['account']
+    enterprise.password = request.form['password']
+    sqlalchemy.session.add(enterprise)
+    sqlalchemy.session.commit()
+    return response_success(True)
