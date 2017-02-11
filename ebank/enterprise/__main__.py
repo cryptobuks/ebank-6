172d114e21e6359aec09d2630a421c9345146df3
7c00 <i@7c00.cc>
2017-02-11 11:52:16 +0800

    指定监听地址

--- ebank/enterprise/__main__.py
+++ ebank/enterprise/__main__.py
@@ -3,4 +3,4 @@ from os.path import dirname, realpath
 sys.path.append(dirname(dirname(dirname(realpath(__file__)))))
 
 from ebank.enterprise import app
-app.run(debug=True, port=8002)
+app.run(debug=True, host='0.0.0.0', port=8002)
